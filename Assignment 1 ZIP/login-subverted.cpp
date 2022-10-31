048e40e37b2e273ab558af9f2e23a8149695a073
veraborvinski <72395680+veraborvinski@users.noreply.github.com>
2022-10-31 01:49:58 +0000

    Update login-subverted.cpp

--- Assignment 1 ZIP/login-subverted.cpp
+++ Assignment 1 ZIP/login-subverted.cpp
@@ -1,3 +1,4 @@
+//include necessary libraries
 #include <iostream>
 #include <iomanip>
 #include <sstream>
@@ -13,6 +14,12 @@ using namespace std;
 string y = "9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08";
 
 //hash function: https://stackoverflow.com/questions/2262386/generate-sha256-with-openssl-and-c
+/**
+ * Uses openssl to hash password in sha-256
+ *
+ * @param string, unhashed password
+ * @return string, hashed password
+ */
 string sha256(const string unhashed)
 {
 
@@ -46,11 +53,19 @@ string sha256(const string unhashed)
     return "";
 }
 
+/**
+ * Checks login details
+ *
+ * @param string username
+ * @return bool, if login was processed
+ */
 bool login(string username){
 
 	string password, file, line;
 	
+	//ask user for login info
 	cout << "enter password" << endl, cin >> password;
+	
 	//this is to check whether password is empty, if yes ask again
 	bool x = false;
     	if(password == "'") {
@@ -58,24 +73,38 @@ bool login(string username){
 	    	//checks if new password is hashable
 	    	sha256(password) == y ? x = true: x = false;
 	}
+	
+	//ask user for file info
 	cout << "enter file" << endl, cin >> file;
-
+	
+	//hash password
 	string h_password = sha256(password);
+	
+	//format username and password like in password file
 	string pair = username + ":" + h_password;
+	
+	//open password file and read from it
 	ifstream Inputfile (file);
 	if ( Inputfile.is_open() ) {
-	//reading from a file https://stackoverflow.com/questions/12463750/c-searching-text-file-for-a-particular-string-and-returning-the-line-number-wh
+	//reading from a file by line: https://stackoverflow.com/questions/12463750/c-searching-text-file-for-a-particular-string-and-returning-the-line-number-wh
 		while(getline(Inputfile, line)) {
+			//return true if line is found and there is a password
 	    		if (line.find(pair, 0) != string::npos || x) return true;
 	    	}
 	}
+	//if user password pair were not in the file, return false
 	return false;
 }
 
+/**
+ * Main function
+ */
 int main() {
 
 	string username;
+	//prompts user to enter username
 	cout << "enter username" << endl, cin >> username;
+	//authenticate user if they were successfully logged in, reject otherwise
   	login(username) ? authenticated(username) : rejected(username);
 }
 

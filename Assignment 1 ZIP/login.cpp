ffb9684f5507929a9f97aaf9d3761fb515c20350
veraborvinski <72395680+veraborvinski@users.noreply.github.com>
2022-10-31 01:40:49 +0000

    Update login.cpp

--- Assignment 1 ZIP/login.cpp
+++ Assignment 1 ZIP/login.cpp
@@ -1,3 +1,7 @@
+//Authors: Vera Tykesson Borvinski, Dimitar Valkov, Duke Woy, Pengfeng Luo, Sheik Mahamud
+//Matriculation numbers: 2421818, 2413179, 2418150, 2458646
+
+//include necessary libraries
 #include <iostream>
 #include <iomanip>
 #include <sstream>
@@ -10,6 +14,12 @@
 using namespace std;
 
 //hash function: https://stackoverflow.com/questions/2262386/generate-sha256-with-openssl-and-c
+/**
+ * Uses openssl to hash password in sha-256
+ *
+ * @param string, unhashed password
+ * @return string, hashed password
+ */
 string sha256(const string unhashed)
 {
 
@@ -43,29 +53,48 @@ string sha256(const string unhashed)
     return "";
 }
 
+/**
+ * Checks login details
+ *
+ * @param string username
+ * @return bool, if login was processed
+ */
 bool login(string username){
 
 	string password, file, line;
 
+	//ask user for login info
 	cout << "enter password" << endl, cin >> password;
 	cout << "enter file" << endl, cin >> file;	
 	
+	//hash password
 	string h_password = sha256(password);
+	
+	//format username and password like in password file
 	string pair = username + ":" + h_password;
+	
+	//open password file and read from it
 	ifstream Inputfile (file);
 	if ( Inputfile.is_open() ) {
-	//reading from a file https://stackoverflow.com/questions/12463750/c-searching-text-file-for-a-particular-string-and-returning-the-line-number-wh
+	//reading from a file line by line: https://stackoverflow.com/questions/12463750/c-searching-text-file-for-a-particular-string-and-returning-the-line-number-wh
 		while(getline(Inputfile, line)) {
+			//return true if value pair was found
 	    		if (line.find(pair, 0) != string::npos) return true;
 	    	}
 	}
+	//if user password pair were not in the file, return false
 	return false;
 }
 
+/**
+ * Main function
+ */
 int main() {
 
 	string username;
+	//prompts user to enter username
 	cout << "enter username" << endl, cin >> username;
+	//authenticate user if they were successfully logged in, reject otherwise
   	login(username) ? authenticated(username) : rejected(username);
 }
 
